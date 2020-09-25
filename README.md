a2f547313169c0263cbf7e9df409651dc9f051f4
MichaelPascale <32907334+MichaelPascale@users.noreply.github.com>
2020-09-25 13:31:17 -0400

    Publishing notes, updated script.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Plea Simulation Animation Assets
+
 This repository contains the original Adobe Animate `.fla`, exported HTML Canvas `.js`, and any image files (background, foreground, cached) for each animated scene of the [plea bargain simulation](https://github.com/Plea-Justice/pleabargain-simulation). A project overview and detailed documentation are available at [pleajustice.org](https://pleajustice.org).
+
+## Developer Notes
+
+* Adobe exports animated assets to CreateJS Canvas. The generated `.js` files include an internal "composition ID" which necesitates a lookup table to match asset name to ID. The publishing script adds a lookup table for this at `window.FILE_TO_ID`. These internal IDs must be unique, though it is not always the case that Animate gives assets unique IDs. For example, if `.fla` is copied to make a similar asset, the resulting `.js` files will have the same internal IDs and will not be able to be loaded by the simulation. To solve this, manually change any identical IDs.
+
+```bash
+$ cat assets/clips/Larceny_FlashbackInnocent.js | grep -o "id: '.*$" | cut -d "'" -f 2
+DB4EA2F77FBC49619D87EB89080157A4
+
+$ cat assets/clips/Larceny_FlashbackGuilty.js | grep -o "id: '.*$" |  cut -d "'" -f 2
+DB4EA2F77FBC49619D87EB89080157A4
+
+$ sed -i 's/DB4EA2F77FBC49619D87EB89080157A4/DB4EA2F77FBC49619D87EB89080157A5/g' Larceny_FlashbackInnocent.js
+```
+
+* Bitmap caching is supported by the simulation code, but separate image files are *not* by the console. Assets that require a separate `images/` directory are problematic to upload to the console and also to load when the simulation is run or when thumbnails are generated. Because of this you may still use bitmap cache on symbols, but you should *disable* 'Export Images' in the [publishing settings](https://helpx.adobe.com/be_en/animate/using/creating-publishing-html5-canvas-document.html#OptimizingHTML5Canvasoutput).
+
+* The publishing script is run automatically by the console when a file is uploaded. If you need to do fine tuned customizations to the `.js` file, you may manually publish using the script in this repository.
+
+* The simulation is no longer backwards compatible with Adobe Animate 20.0.3 or below. All assets must be exported with Animate >=20.0.4.
--- publish.js
+++ publish.js
@@ -9,113 +9,112 @@
 const fs = require('fs');
 const path = require('path');
 
-let figures = 2;
-let palettables = {
-    "#666600":"eyeA",
-    "#663300":"hairA",
-    "#FFCC99":"skinA",
-    "#F49E50":"skinB",
-    "#E5CCFF":"outfitA",
-    "#70618D":"outfitB"
-}
-
 let option = false;
 let force = false;
-let nocopy = false;
-let cachedir = false;
+let copy = false;
 
+// From researcher-console/server/common/util.js.
+function multipleReplace(string, replacements) {
+    for (const [regex, newstr] of replacements)
+        string = string.replace(regex, newstr);
+    return string;
+}
 
-try {
-    // Parse arguments.
-    if (process.argv.length < 3) throw Error('No file specified.');
+/**
+ * Prepare an Animate asset for use with the simulation by inserting variables.
+ * 
+ * Modified assets will expect a window.assetPalettes to be defined. May throw
+ * errors on unsuccessful read/write, if the asset has already been run through
+ * the function, or if the extension of the file is not '.js'. The passed file
+ * will be overwritten.
+ * 
+ * @param {String} input The path to the input file.
+ */
+function publish(input) {
 
-    if (process.argv[2].startsWith('-')) {
-        option = true;
-        if (process.argv[2].includes('f')) force = true;
-        if (process.argv[2].includes('n')) nocopy = true;
-        if (process.argv[2].includes('c')) cachedir = true;
-        if (process.argv.length < 4) throw Error('Option specified with no file.');
-    }
-    
-    let file = path.parse(process.argv[option ? 3 : 2]);
-    let filepath = path.format(file);
+    const file = path.parse(input);
+    const filepath = path.format(file);
 
-    if (file.ext !== '.js') throw Error('File must be of type \'.js\'.')
+    if (file.ext !== '.js') throw Error('File must be of type \'.js\'.');
 
     // Check if file has already been published.
     let data = fs.readFileSync(filepath, {encoding: 'utf-8'});
-    if (!force && (data.startsWith('// Published.') || data.endsWith('// Published.')))
-        throw Error('File already marked as published.');
+    if (!force && (data.startsWith('// Published.') || data.endsWith('// Published.'))) {
+
+        if (require.main === module) {
+            throw Error('File already marked as published.');
+        } else {
+            console.log('File already marked as published.');
+            return;
+        }
+    }
     
     // Make a backup copy of the original file.
-    if (!nocopy)
+    if (copy)
         fs.copyFileSync(filepath, `${file.name}.orig${file.ext}`);
     
-    // Define a local reference to avatarPalette.
-    data = data.replace(
-        /^\/\/ stage content:\n.*function\(mode,startPosition,loop.*\).*$/gm,
-        '$&\nthis.assetPalette = avatarPalette;\n'
-    );
+    const replacements = [
+        [/"#ACAC3(\d)"/gm, 'window.assetPalettes[$1].colors[5]'],
+        [/"#AC9C3(\d)"/gm, 'window.assetPalettes[$1].colorsDark[5]'],
+        [/"#AC3C3(\d)"/gm, 'window.assetPalettes[$1].colors[3]'],
+        [/"#3C3CA(\d)"/gm, 'window.assetPalettes[$1].colors[0]'],
+        [/"#AC3CA(\d)"/gm, 'window.assetPalettes[$1].colors[4]'],
+        [/"#AC2CA(\d)"/gm, 'window.assetPalettes[$1].colorsDark[4]'],
+        
+        // TODO: Assign all colors slot names instead (color1, color2, etc.) and make dynamic.
+        [/"#3CAC3(\d)"/gm, 'window.assetPalettes[$1].colors[1]'],
+        [/"#3CACA(\d)"/gm, 'window.assetPalettes[$1].colors[2]'],
     
-    // Replace special colors with references to avatar palette colors.
-    for (const color in palettables) {
-        data = data.replace(
-            RegExp(`graphics\\.f\\("${color}"\\)\\.s\\(\\)`, 'gm'),
-            `graphics.f(this.assetPalette.${palettables[color]}).s()`
-        );
-    }
+        // Customizable features (hair, eyes, etc).
+        [/(slot(\d)figure(\d)([a-z]+?)(?<!accessory)(\d)[\s\S]*?)(^.*addTween)/gm,
+            '$1if (window.assetPalettes[$2].features.figure === $3 && window.assetPalettes[$2].features.$4 === $5)$6'
+        ],
+    
+        // Base layers and accessories.
+        // Accessory layer has number, but number is unused in selection. Only figure.
+        [/(slot(\d)figure(\d)[a-z\d]+?\s[\s\S]*?)(^.*addTween)/gm,
+            '$1if (window.assetPalettes[$2].features.figure === $3)$4'
+        ],
+    
+        // Reference to cache directory for bitmap cached assets.
+        [/"images\//g, '"assets/cache/'],
+    
+        // Lookup table to retrieve the composition ID from the filename.
+        [/^}\)\(createjs = createjs\|\|{}, AdobeAn = AdobeAn\|\|{}\);$/gm,
+            `\nFILE_TO_ID = window.FILE_TO_ID || {}; FILE_TO_ID["${file.name}"] = lib.properties.id;\n$&`
+        ]
+    ];
 
-    // Add toggling if-statement to avatar figure layers.
-    for (let i = 0; i < figures; ++i) {
-        figure = `figure${i}`;
-
-        data = data.replace(
-            RegExp( `(\\/\\/ ${figure}avatar(.|\\n)*?)(^.*addTween)`, 'gm' ),
-            `$1if (this.assetPalette.figure == ${i})$3`
-        );
-
-        data = data.replace(
-            RegExp( `(\\/\\/ ${figure}hair(\\d)(.|\\n)*?)(^.*addTween)`, 'gm' ),
-            `$1if (this.assetPalette.figure == ${i} && this.assetPalette.hair == $2)$4`
-        );
-
-        data = data.replace(
-            RegExp( `(\\/\\/ ${figure}eyes(\\d)(.|\\n)*?)(^.*addTween)`, 'gm' ),
-            `$1if (this.assetPalette.figure == ${i} && this.assetPalette.eyes == $2)$4`
-        );
-
-        // Fix for separate glasses layer on figures. Supports accessory layers on figure.
-        data = data.replace(
-            RegExp( `(\\/\\/ ${figure}accessory\\d(.|\\n)*?)(^.*addTween)`, 'gm' ),
-            `$1if (this.assetPalette.figure == ${i})$3`
-        );
-    }
-
-    // Replace references to cached bitmap images/ directory with assets/cached.
-    if (!cachedir)
-        data = data.replace(
-            /"images\//,
-            '"assets/cached/'
-        );
-
-    // Insert a lookup table entry so the composition ID may be found later.
-    data = data.replace(
-        /^}\)\(createjs = createjs\|\|{}, AdobeAn = AdobeAn\|\|{}\);$/gm,
-        `\nFILE_TO_ID = window.FILE_TO_ID || {}; FILE_TO_ID["${file.name}"] = lib.properties.id;\n$&`
-    );
-
+    data = multipleReplace(data, replacements);
+    
     // Mark file as published and write.
-    data += '\n// Published.'
+    data += '\n// Published.';
     fs.writeFileSync(filepath, data);
-
-    console.log('\033[33mPublished!\033[m');
-
-} catch(err) {
-    console.log('\033[31m' + `\nError: ${err.message}` + '\033[m\n');
-    console.log('Usage:    node publish.js <options> <filename>\n');
-    console.log('Options:  -fna');
-    console.log('           f   Force publishing if file has already been published.');
-    console.log('            n  Do not make a backup copy of the original file.');
-    console.log('             a Do not replace references to cached bitmap images/ with assets/cached/.');
-    process.exit(1);
 }
+
+if (require.main === module) {
+    try {
+        // Parse arguments.
+        if (process.argv.length < 3) throw Error('No file specified.');
+
+        if (process.argv[2].startsWith('-')) {
+            option = true;
+            if (process.argv[2].includes('f')) force = true;
+            if (process.argv[2].includes('c')) copy = true;
+            if (process.argv.length < 4) throw Error('Option specified with no file.');
+        }
+
+        publish(process.argv[option ? 3 : 2]);
+
+        console.log('\033[33mPublished!\033[m');
+    } catch(err) {
+        console.log('\033[31m' + `\nError: ${err.message}` + '\033[m\n');
+        console.log('Usage:    node publish.js <options> <filename>\n');
+        console.log('Options:  -fc');
+        console.log('           f   Force publishing if file has already been published.');
+        console.log('           c   Make a backup copy of the original file.');
+        process.exit(1);
+    }
+}
+
+module.exports = {publish};
